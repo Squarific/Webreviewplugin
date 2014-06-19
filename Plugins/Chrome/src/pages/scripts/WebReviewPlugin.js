219b97d1b6293ce7476ef35b60657c6f761e1ad4
Filip Smets <alemaaltevinden@gmail.com>
2014-06-19 12:48:19 +0200

    Changed to windows end line

--- Plugins/Chrome/src/pages/scripts/WebReviewPlugin.js
+++ Plugins/Chrome/src/pages/scripts/WebReviewPlugin.js
@@ -1,1 +1,122 @@
-function WebReviewPlugin (server) {	this.server = server;}WebReviewPlugin.prototype.login = function (email, password, callback) {	var params = "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);	var request = new XMLHttpRequest();	request.addEventListener("error", function () {		callback('{"error": "Failed to login, do you have an internet connection?"}');	});	request.addEventListener("load", function () {		var response = JSON.parse(request.responseText);		if (response.email) {			localStorage.email = response.email;		}		callback(request.responseText);	}, false);	request.open("POST", this.server + "/login", true);	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");	request.send(params);};WebReviewPlugin.prototype.postReview = function postReview (form, callback) {	var params = [];	for (var k in form) {		params.push(k + "=" + form[k]);	}	var params = params.join("&");	var request = new XMLHttpRequest();	request.addEventListener("error", function () {		callback('{"error": "Failed to post review, do you have an internet connection?"}');	});	request.addEventListener("load", function () {		callback(request.responseText);	}, false);	request.open("POST", this.server + "/addreview", true);	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");	request.send(params);};WebReviewPlugin.prototype.getReview = function getReview (id, callback) {	var request = new XMLHttpRequest();	request.addEventListener("error", function () {		callback({error: "Failed to load review, do you have an internet connection?"});	});	request.addEventListener("load", function () {		callback(JSON.parse(request.responseText));	}, false);	request.open("GET", this.server + "/review/" + id);	request.send();};WebReviewPlugin.prototype.search = function search (query, callback) {	var request = new XMLHttpRequest();	request.addEventListener("error", function () {		callback({error: "Failed to load reviews, do you have an internet connection?"});	});	request.addEventListener("load", function () {		callback(JSON.parse(request.responseText));	}, false);	request.open("GET", this.server + "/search/" + encodeURIComponent(query));	request.send();};WebReviewPlugin.prototype.getReviews = function getReviews (domain, callback) {	var request = new XMLHttpRequest();	request.addEventListener("error", function () {		callback({error: "Failed to load reviews, do you have an internet connection?"});	});	request.addEventListener("load", function () {		callback(JSON.parse(request.responseText));	}, false);	request.open("GET", this.server + "/domain/" + encodeURIComponent(domain));	request.send();};WebReviewPlugin.prototype.getDomReview = function (jsonReview) {	var review = document.createElement("div");	review.className = "smallreview";		review.appendChild(this.newDomSpan(jsonReview.reviewedDomain, ""));	review.appendChild(document.createElement("br"));		review.appendChild(this.newDomSpan("Date: ", "reviewlabel label"));	review.appendChild(this.newDomSpan(new Date(jsonReview.reviewedTime).toLocaleString(), ""));	review.appendChild(document.createElement("br"));		review.appendChild(this.newDomSpan("Desc: ", "reviewlabel label"));	review.appendChild(this.newDomSpan(jsonReview.shortDescription, ""));	review.appendChild(document.createElement("br"));		if (jsonReview.longDescription) {		review.appendChild(this.newDomSpan("Full desc: ", "reviewlabel label"));				var longDescDiv = review.appendChild(document.createElement("div"));		longDescDiv.innerHTML = markdown.toHTML(jsonReview.longDescription);				review.appendChild(document.createElement("br"));	} else {		review.appendChild(this.newDomMoreInfoButton(jsonReview.reviewId));	}		return review;};WebReviewPlugin.prototype.newDomSpan = function (innerText, className) {	var span = document.createElement("span");	span.appendChild(document.createTextNode(innerText));	span.className = className;	return span;};WebReviewPlugin.prototype.newDomMoreInfoButton = function (id) {	var anchor = document.createElement("a");	anchor.href = "review.html?id=" + id;	var button = anchor.appendChild(document.createElement("div"));	button.className = "moreinfobutton button";	button.innerText = "More about this review";	return anchor;};var reviewPlugin = new WebReviewPlugin("http://reviews.squarific.com");
+function WebReviewPlugin (server) {
+	this.server = server;
+}
+
+WebReviewPlugin.prototype.login = function (email, password, callback) {
+	var params = "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
+	var request = new XMLHttpRequest();
+	request.addEventListener("error", function () {
+		callback('{"error": "Failed to login, do you have an internet connection?"}');
+	});
+	request.addEventListener("load", function () {
+		var response = JSON.parse(request.responseText);
+		if (response.email) {
+			localStorage.email = response.email;
+		}
+		callback(request.responseText);
+	}, false);
+	request.open("POST", this.server + "/login", true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.send(params);
+};
+
+WebReviewPlugin.prototype.postReview = function postReview (form, callback) {
+	var params = [];
+	for (var k in form) {
+		params.push(k + "=" + form[k]);
+	}
+	var params = params.join("&");
+	var request = new XMLHttpRequest();
+	request.addEventListener("error", function () {
+		callback('{"error": "Failed to post review, do you have an internet connection?"}');
+	});
+	request.addEventListener("load", function () {
+		callback(request.responseText);
+	}, false);
+	request.open("POST", this.server + "/addreview", true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.send(params);
+};
+
+WebReviewPlugin.prototype.getReview = function getReview (id, callback) {
+	var request = new XMLHttpRequest();
+	request.addEventListener("error", function () {
+		callback({error: "Failed to load review, do you have an internet connection?"});
+	});
+	request.addEventListener("load", function () {
+		callback(JSON.parse(request.responseText));
+	}, false);
+	request.open("GET", this.server + "/review/" + id);
+	request.send();
+};
+
+WebReviewPlugin.prototype.search = function search (query, callback) {
+	var request = new XMLHttpRequest();
+	request.addEventListener("error", function () {
+		callback({error: "Failed to load reviews, do you have an internet connection?"});
+	});
+	request.addEventListener("load", function () {
+		callback(JSON.parse(request.responseText));
+	}, false);
+	request.open("GET", this.server + "/search/" + encodeURIComponent(query));
+	request.send();
+};
+
+WebReviewPlugin.prototype.getReviews = function getReviews (domain, callback) {
+	var request = new XMLHttpRequest();
+	request.addEventListener("error", function () {
+		callback({error: "Failed to load reviews, do you have an internet connection?"});
+	});
+	request.addEventListener("load", function () {
+		callback(JSON.parse(request.responseText));
+	}, false);
+	request.open("GET", this.server + "/domain/" + encodeURIComponent(domain));
+	request.send();
+};
+
+WebReviewPlugin.prototype.getDomReview = function (jsonReview) {
+	var review = document.createElement("div");
+	review.className = "smallreview";
+	
+	review.appendChild(this.newDomSpan(jsonReview.reviewedDomain, ""));
+	review.appendChild(document.createElement("br"));
+	
+	review.appendChild(this.newDomSpan("Date: ", "reviewlabel label"));
+	review.appendChild(this.newDomSpan(new Date(jsonReview.reviewedTime).toLocaleString(), ""));
+	review.appendChild(document.createElement("br"));
+	
+	review.appendChild(this.newDomSpan("Desc: ", "reviewlabel label"));
+	review.appendChild(this.newDomSpan(jsonReview.shortDescription, ""));
+	review.appendChild(document.createElement("br"));
+	
+	if (jsonReview.longDescription) {
+		review.appendChild(this.newDomSpan("Full desc: ", "reviewlabel label"));
+		
+		var longDescDiv = review.appendChild(document.createElement("div"));
+		longDescDiv.innerHTML = markdown.toHTML(jsonReview.longDescription);
+		
+		review.appendChild(document.createElement("br"));
+	} else {
+		review.appendChild(this.newDomMoreInfoButton(jsonReview.reviewId));
+	}
+	
+	return review;
+};
+
+WebReviewPlugin.prototype.newDomSpan = function (innerText, className) {
+	var span = document.createElement("span");
+	span.appendChild(document.createTextNode(innerText));
+	span.className = className;
+	return span;
+};
+
+WebReviewPlugin.prototype.newDomMoreInfoButton = function (id) {
+	var anchor = document.createElement("a");
+	anchor.href = "review.html?id=" + id;
+	var button = anchor.appendChild(document.createElement("div"));
+	button.className = "moreinfobutton button";
+	button.innerText = "More about this review";
+	return anchor;
+};
+
+var reviewPlugin = new WebReviewPlugin("http://reviews.squarific.com");
--- Plugins/Chrome/src/pages/scripts/browser_action_popup.js
+++ Plugins/Chrome/src/pages/scripts/browser_action_popup.js
@@ -1,1 +1,54 @@
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {	var domain = new URL(tabs[0].url).hostname;	document.getElementById("addreviewlink").href = "add_review.html?domain=" + encodeURIComponent(domain);	document.getElementById("pagestatusdomain").innerText = domain;	reviewPlugin.getReviews(domain, function (reviews) {		var msg = document.getElementById("message");		var modadded, comadded;		while (msg.firstChild) {			msg.removeChild(msg.firstChild);			msg.style.display = "none";		}		if (reviews.error) {			var msg = document.getElementById("message");			msg.style.display = "inline-block";			var h3 = msg.appendChild(document.createElement("h3"));			h3.appendChild(document.createTextNode(reviews.error));			return;		}		if (reviews.length === 0) {			var div = document.getElementById("modreviews");			div.parentNode.removeChild(div);			var div = document.getElementById("communityreviews");			div.parentNode.removeChild(div);			var msg = document.getElementById("message");			msg.style.display = "inline-block";			var h3 = msg.appendChild(document.createElement("h3"));			h3.appendChild(document.createTextNode("Noone has reviewed this website yet."));		}		for (var k = 0; k < reviews.length; k++) {			var target = (reviews[k].modVerified) ? document.getElementById("modreviews") : document.getElementById("communityreviews");			if (reviews[k].modVerified) {				if (!modadded) {					var div = document.getElementById("modreviews");					var h3 = div.appendChild(document.createElement("h3"));					h3.appendChild(document.createTextNode("Approved reviews."));					modadded = true;				}			} else {				if (!comadded) {					var div = document.getElementById("communityreviews");					var h3 = div.appendChild(document.createElement("h3"));					h3.appendChild(document.createTextNode("Community reviews."));					comadded = true;				}			}			target.appendChild(reviewPlugin.getDomReview(reviews[k]));		}	});});var removeList = document.getElementsByClassName("remove");while (removeList[0]) {	removeList[0].parentNode.removeChild(removeList[0]);}
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	var domain = new URL(tabs[0].url).hostname;
+	document.getElementById("addreviewlink").href = "add_review.html?domain=" + encodeURIComponent(domain);
+	document.getElementById("pagestatusdomain").innerText = domain;
+	reviewPlugin.getReviews(domain, function (reviews) {
+		var msg = document.getElementById("message");
+		var modadded, comadded;
+		while (msg.firstChild) {
+			msg.removeChild(msg.firstChild);
+			msg.style.display = "none";
+		}
+		if (reviews.error) {
+			var msg = document.getElementById("message");
+			msg.style.display = "inline-block";
+			var h3 = msg.appendChild(document.createElement("h3"));
+			h3.appendChild(document.createTextNode(reviews.error));
+			return;
+		}
+		if (reviews.length === 0) {
+			var div = document.getElementById("modreviews");
+			div.parentNode.removeChild(div);
+			var div = document.getElementById("communityreviews");
+			div.parentNode.removeChild(div);
+			var msg = document.getElementById("message");
+			msg.style.display = "inline-block";
+			var h3 = msg.appendChild(document.createElement("h3"));
+			h3.appendChild(document.createTextNode("Noone has reviewed this website yet."));
+		}
+		for (var k = 0; k < reviews.length; k++) {
+			var target = (reviews[k].modVerified) ? document.getElementById("modreviews") : document.getElementById("communityreviews");
+			if (reviews[k].modVerified) {
+				if (!modadded) {
+					var div = document.getElementById("modreviews");
+					var h3 = div.appendChild(document.createElement("h3"));
+					h3.appendChild(document.createTextNode("Approved reviews."));
+					modadded = true;
+				}
+			} else {
+				if (!comadded) {
+					var div = document.getElementById("communityreviews");
+					var h3 = div.appendChild(document.createElement("h3"));
+					h3.appendChild(document.createTextNode("Community reviews."));
+					comadded = true;
+				}
+			}
+			target.appendChild(reviewPlugin.getDomReview(reviews[k]));
+		}
+	});
+});
+
+var removeList = document.getElementsByClassName("remove");
+while (removeList[0]) {
+	removeList[0].parentNode.removeChild(removeList[0]);
+}
